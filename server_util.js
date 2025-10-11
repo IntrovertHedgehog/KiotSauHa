8d55379686dc091eb299a2e5476da15c74ff7e22
IntrovertHedgehog <larby2409@gmail.com>
2025-10-11 14:00:19 +0800

    with util

---
+++ server_util.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken")
+
+module.exports.verify_token = function(req, res) {
+  console.log(req.headers)
+  const token = req.header("authorization");
+  if (!token) {
+    res.status(401).send("please log in");
+  } else {
+    try {
+      jwt.verify(token, process.env.SECRET_TOKEN);
+      console.log("toi day roi")
+      return true;
+    } catch (err) {
+      console.error(err)
+      res.status(401).send("Phiên đăng nhập đã hết hạn, vui lòng đăng nhập lại")
+    }
+  }
+}
