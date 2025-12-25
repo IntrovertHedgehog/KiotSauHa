17742015db28f97c19940c6f4b0fc0d24b4644a5
Admin <larby2409@gmail.com>
2025-12-25 15:22:03 +0700

    add debug file

---
+++ .zed/debug.js
@@ -0,0 +1,10 @@
+[
+  {
+    adapter: "JavaScript",
+    label: "Debug JS file",
+    type: "node",
+    request: "launch",
+    program: "$ZED_FILE",
+    skipFiles: ["<node_internals>/**"],
+  },
+];
